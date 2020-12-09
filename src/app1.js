7e942247cdd08020f2704fd1aac8886955064fa7
Pancake <834430775@qq.com>
2020-12-09 14:03:39 +0800

    MVC

---
+++ src/app1.js
@@ -0,0 +1,35 @@
+import './app1.css'
+import $ from 'jquery'
+
+const $button1 = $('#add1')
+const $button2 = $('#min1')
+const $button3 = $('#mul1')
+const $button4 = $('#dev1')
+const $number = $('#number')
+const n = localStorage.getItem('n')
+$number.text(n || 100)
+
+$button1.on('click',()=>{
+    let n = parseInt($number.text());
+    n+=1;
+    localStorage.getItem('n',n)
+    $number.text(n);
+})
+$button2.on('click',()=>{
+    let n = parseInt($number.text());
+    n-=1;
+    localStorage.getItem('n',n)
+    $number.text(n);
+})
+$button3.on('click',()=>{
+    let n = parseInt($number.text());
+    n *= 2;
+    localStorage.getItem('n',n)
+    $number.text(n);
+})
+$button4.on('click',()=>{
+    let n = parseInt($number.text());
+    n /= 2;
+    localStorage.getItem('n',n)
+    $number.text(n);
+})
